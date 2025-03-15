bb7b560bd76da0aa6501c711d9c56ab3a7337ab7
EECS3311ZGroup1 <gtan95@my.yorku.ca>
2025-03-15 17:42:53 -0400

    Implemented ParkingSpacePublisher

--- src/main/java/yorku/eecs3311/ParkingSpacePublisher.java
+++ src/main/java/yorku/eecs3311/ParkingSpacePublisher.java
@@ -1,5 +1,32 @@
 package yorku.eecs3311;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingSpacePublisher {
+	private List<ParkingSpaceObserver> _observers = new ArrayList<ParkingSpaceObserver>();
+	private List<ParkingComponent> _parkingSpots;
+	public List<ParkingComponent> getParkingSpots(){
+		return this._parkingSpots;
+	}
+	
+	public void addParkingSpot(ParkingComponent parkingSpot) {
+		this._parkingSpots.add(parkingSpot);
+		this.notifyAllObservers();
+	}
+	
+	public void removeParkingSpot(ParkingComponent parkingSpot) {
+		this._parkingSpots.remove(parkingSpot);
+		this.notifyAllObservers();
+	}
+	
+	public void attachObserver(ParkingSpaceObserver observer) {
+		this._observers.add(observer);
+	}
+	
+	public void notifyAllObservers() {
+		for(ParkingSpaceObserver observer: _observers) {
+			observer.update();
+		}
+	}
 	
 }
