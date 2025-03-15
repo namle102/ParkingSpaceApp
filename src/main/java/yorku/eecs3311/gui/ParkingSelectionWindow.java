e5169400240af5ae37b4a353f5ba3ca61fad9714
EECS3311ZGroup1 <gtan95@my.yorku.ca>
2025-03-15 14:45:17 -0400

    Added list example implementation

---
+++ src/main/java/yorku/eecs3311/gui/ParkingSelectionWindow.java
@@ -0,0 +1,48 @@
+package yorku.eecs3311.gui;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+
+import yorku.eecs3311.ParkingSpace;
+import yorku.eecs3311.gui.list.ListElement;
+
+public class ParkingSelectionWindow {
+	private JFrame window;
+	private JList<ListElement<ParkingSpace>> parkingSpaceList;
+	JPanel panel = new JPanel();
+	DefaultListModel<ListElement<ParkingSpace>> model;
+	JSplitPane splitPane = new JSplitPane();
+	
+	public ParkingSelectionWindow() {
+		window = new JFrame();
+		parkingSpaceList = new JList<ListElement<ParkingSpace>>();
+		model = new DefaultListModel<>();
+		
+		model.addElement(new ListElement<ParkingSpace>("ParkingSpace1", new ParkingSpace()));
+		model.addElement(new ListElement<ParkingSpace>("ParkingSpace2", new ParkingSpace()));
+		model.addElement(new ListElement<ParkingSpace>("ParkingSpace3", new ParkingSpace()));
+		
+		
+		window.setTitle("York Parking Space Selection");
+		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);;
+		window.setSize(800, 500);
+		window.setLocationRelativeTo(null);
+		
+		parkingSpaceList.setModel(model);
+		
+		splitPane.setLeftComponent(new JScrollPane(parkingSpaceList));
+		splitPane.setRightComponent(panel);
+		
+		window.add(splitPane);
+		
+		
+	}
+	
+	public void show() {
+		window.setVisible(true);
+	}
+}
