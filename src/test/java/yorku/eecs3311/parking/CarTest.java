5384f31eec9360dfe410ffe827d580fd48e78e2e
TheFoolshub <ali2021@my.yorku.ca>
2025-04-05 19:59:45 -0400

    Update CarTest.java
    updated CarTest.java  Req 7

--- src/test/java/yorku/eecs3311/parking/CarTest.java
+++ src/test/java/yorku/eecs3311/parking/CarTest.java
@@ -1,5 +1,78 @@
 package yorku.eecs3311.parking;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CarTest {
 
+    private Car car;
+
+    @Before
+    public void setUp() {
+        car = new Car("Honda", "Civic", "DEF456");
+    }
+
+    @Test
+    public void testCarConstructor() {
+        assertEquals("Honda", car.getMake());
+        assertEquals("Civic", car.getModel());
+        assertEquals("DEF456", car.getPlateNumber());
+    }
+
+    @Test
+    public void testPark() {
+        ParkingSpace mockSpace = new ParkingSpace(1, "LotA");
+        car.park(mockSpace);
+        assertTrue(mockSpace.getSensor().isCarParked());
+    }
+
+    @Test
+    public void testUnpark() {
+        ParkingSpace mockSpace = new ParkingSpace(1, "LotA");
+        car.park(mockSpace);
+        car.unpark(mockSpace);
+        assertFalse(mockSpace.getSensor().isCarParked());
+    }
+
+    @Test
+    public void testGetMake() {
+        assertEquals("Honda", car.getMake());
+    }
+
+    @Test
+    public void testSetMake() {
+        car.setMake("Toyota");
+        assertEquals("Toyota", car.getMake());
+    }
+
+    @Test
+    public void testGetModel() {
+        assertEquals("Civic", car.getModel());
+    }
+
+    @Test
+    public void testSetModel() {
+        car.setModel("Corolla");
+        assertEquals("Corolla", car.getModel());
+    }
+
+    @Test
+    public void testGetPlateNumber() {
+        assertEquals("DEF456", car.getPlateNumber());
+    }
+
+    @Test
+    public void testSetPlateNumber() {
+        car.setPlateNumber("XYZ789");
+        assertEquals("XYZ789", car.getPlateNumber());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "Car [make=Honda, model=Civic, plateNumber=DEF456]";
+        assertEquals(expected, car.toString());
+    }
 }
+
