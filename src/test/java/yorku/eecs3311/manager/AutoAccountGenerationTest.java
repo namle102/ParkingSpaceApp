823bf5e351411efd9a2f782ed083ad17dbaa6e81
EECS3311ZGroup1 <gtan95@my.yorku.ca>
2025-04-05 12:04:58 -0400

    Added AutoAccountGeneration test

--- src/test/java/yorku/eecs3311/manager/AutoAccountGenerationTest.java
+++ src/test/java/yorku/eecs3311/manager/AutoAccountGenerationTest.java
@@ -1,5 +1,21 @@
 package yorku.eecs3311.manager;
 
-public class AutoAccountGenerationTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class AutoAccountGenerationTest {
+	
+	@Test
+	public void generateMangerAccountTest() {
+		AutoAccountGeneration auto = new AutoAccountGeneration();
+		auto.generateManagerAccount();
+		List<ManagerAccount> managers = auto.loadManagers();
+		assertEquals(false, managers.isEmpty());
+		
+	}
 }
