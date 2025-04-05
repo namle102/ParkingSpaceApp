e1f289af587724b709e28d40036d00de7a3d1612
EECS3311ZGroup1 <gtan95@my.yorku.ca>
2025-04-05 11:55:01 -0400

    Added manager unit test

--- src/test/java/yorku/eecs3311/manager/ManagerAccountTest.java
+++ src/test/java/yorku/eecs3311/manager/ManagerAccountTest.java
@@ -1,5 +1,246 @@
 package yorku.eecs3311.manager;
 
-public class ManagerAccountTest {
+import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import yorku.eecs3311.Subscriber;
+import yorku.eecs3311.parking.ParkingLot;
+import yorku.eecs3311.parking.ParkingSpace;
+
+class SubcriberRealize implements Subscriber{
+	
+	boolean isUpdated = false;
+	
+	public SubcriberRealize() {
+		
+	}
+	
+	@Override
+	public void update(List<ParkingSpace> availableSpaces) {
+		isUpdated = true;
+		
+	}
+	
+}
+
+public class ManagerAccountTest {
+	private ByteArrayOutputStream outContent = null;
+ 	private final PrintStream originalOut = System.out;
+ 	private final PrintStream originalErr = System.err;
+ 	
+ 	public String get_std_out() {
+ 		return outContent.toString().replace("\n", "").replace("\r", "");
+ 	}
+ 	
+ 	@Before
+ 	public void setupTest() {
+		redirectStream();
+ 	}
+ 	
+ 	public void redirectStream() {
+ 		outContent = new ByteArrayOutputStream();
+ 	    System.setOut(new PrintStream(outContent));
+ 	}
+ 	
+	@Test
+ 	public void addLotTest() {
+		
+		String lotName = "Lot1";
+
+	 	ManagerAccount manager = new ManagerAccount("","");
+	 	
+	 	redirectStream();
+		
+		manager.addLot(lotName);
+ 
+		assertEquals("[+] Parking Lot Added: " + lotName, get_std_out());
+ 		
+	}
+	
+	@Test
+ 	public void addDuplicateLotTest() {
+		
+		String lotName = "Lot2";
+		
+	 	ManagerAccount manager = new ManagerAccount("","");
+	 	
+	 	redirectStream();
+	 	
+	 	manager.addLot(lotName);
+ 
+		assertEquals("[+] Parking Lot Added: " + lotName, get_std_out());
+		
+		redirectStream();
+		
+		manager.addLot(lotName);
+		
+		assertEquals("[-] Lot Already Exists: " + lotName, get_std_out());
+ 		
+	}
+	
+	@Test
+	public void getLotsTest() {
+	 	ManagerAccount manager = new ManagerAccount("","");
+		
+		List<ParkingLot> lots = ManagerAccount.getLots();
+		
+		ArrayList<String> lotExpect = new ArrayList<String>();
+		
+		lotExpect.add("A");
+		lotExpect.add("B");
+		
+		for(int i = 0; i < 2; i++) {
+			assertEquals(lotExpect.get(i), lots.get(i).getLotName());
+			manager.addLot("Lot"+String.valueOf(i));
+		}
+	}
+	
+	@Test
+	public void disableLotTest() {
+	 	ManagerAccount manager = new ManagerAccount("","");
+	 	
+	 	List<ParkingLot> lots = ManagerAccount.getLots();
+	 	
+	 	assertEquals( true,lots.get(0).isEnabled());
+		
+	 	manager.disableLot("A");
+	 	
+	 	assertEquals(false,lots.get(0).isEnabled());
+	 	
+	}
+	
+	@Test
+	public void enableLotTest() {
+	 	ManagerAccount manager = new ManagerAccount("","");
+	 	
+	 	List<ParkingLot> lots = ManagerAccount.getLots();
+		
+	 	manager.disableLot("A");
+	 	
+	 	assertEquals(false,lots.get(0).isEnabled());
+		
+	 	manager.enableLot("A");
+	 	
+	 	assertEquals(true,lots.get(0).isEnabled());
+	 	
+	}
+	
+	@Test
+	public void getLotByNameTest() {	 	
+	 	ParkingLot lot = ManagerAccount.getLotByName("A");
+	 	
+	 	assertNotNull(lot);
+
+	 	lot = ManagerAccount.getLotByName("Z");
+	 	
+	 	assertNull(lot);
+	}
+	
+	@Test
+	public void disableSpaceTest() {
+		List<ParkingSpace> spaces = ManagerAccount.getAvailableSpaces();
+		
+		ParkingSpace space = spaces.get(0);
+		
+		ManagerAccount manager = new ManagerAccount("","");
+		
+		manager.disableSpace("A",space.getSpaceID());
+		
+		assertEquals( false,space.isEnabled());
+		
+		
+		
+	}
+	
+	@Test
+	public void enableSpaceTest() {
+		List<ParkingSpace> spaces = ManagerAccount.getAvailableSpaces();
+		
+		ParkingSpace space = spaces.get(0);
+		
+		ManagerAccount manager = new ManagerAccount("","");
+		
+		manager.disableSpace("A",space.getSpaceID());
+		
+		assertEquals(false,space.isEnabled());
+		
+		manager.enableSpace("A",space.getSpaceID());
+		
+		assertEquals(true,space.isEnabled());
+		
+	}
+	
+	@Test
+	public void toStringTest() {
+		String username = "username1";
+		String pwd = "password1";
+		ManagerAccount manager = new ManagerAccount(username, pwd);
+		assertEquals("ManagerAccount [username=" + username + ", pwd=" + pwd + "]",manager.toString());
+	}
+	
+
+	@Test
+	public void getUsernameTest() {
+		String username = "username1";
+		String pwd = "password1";
+		ManagerAccount manager = new ManagerAccount(username, pwd);
+		assertEquals(username,manager.getUsername());
+	}
+	
+
+	@Test
+	public void getPasswordTest() {
+		String username = "username1";
+		String pwd = "password1";
+		ManagerAccount manager = new ManagerAccount(username, pwd);
+		assertEquals(pwd,manager.getPwd());
+	}
+	
+	@Test
+	public void notifySubscriberTest()
+	{
+		SubcriberRealize sub = new SubcriberRealize();
+		
+		assertEquals(false, sub.isUpdated);
+		
+		ManagerAccount.addSubscriber(sub);
+		
+		ManagerAccount.notifySubscribers();
+		
+		assertEquals(true, sub.isUpdated);
+		
+	}
+	
+
+	@Test
+	public void removeSubscriberTest()
+	{
+		SubcriberRealize sub = new SubcriberRealize();
+		
+		ManagerAccount.addSubscriber(sub);
+		
+		ManagerAccount.removeSubscriber(sub);
+		
+		ManagerAccount.notifySubscribers();
+		
+		assertEquals(false, sub.isUpdated);
+		
+	}
+ 	
+	@After
+ 	public void restoreStreams() {
+		System.out.flush();
+ 	    System.setOut(originalOut);
+ 	    System.setErr(originalErr);
+ 	}
+	
+	
 }
