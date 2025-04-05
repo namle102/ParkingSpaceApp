13a8a4b2a65ddd71f18972185336f3ed33b55206
EECS3311ZGroup1 <gtan95@my.yorku.ca>
2025-04-05 12:10:45 -0400

    Added supermanager test

--- src/test/java/yorku/eecs3311/manager/SuperManagerTest.java
+++ src/test/java/yorku/eecs3311/manager/SuperManagerTest.java
@@ -1,5 +1,21 @@
 package yorku.eecs3311.manager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 public class SuperManagerTest {
-
+	
+	@Test
+	public void SuperManagerInitTest() {
+		SuperManager sm = SuperManager.getInstance();
+		
+		sm.generateManagerAccount();
+		
+		List<ManagerAccount> managers = sm.getManagers();
+		assertEquals(false, managers.isEmpty());
+	}
 }
